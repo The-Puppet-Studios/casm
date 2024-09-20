211f0c882752fde0df2d812fbe8efb7960e865e7
Puppet <doggyandkira2013@gmail.com>
2024-09-19 23:26:08 -0700

    Added option to compile to windows in compile.sh (EXPERIMENTAL)
    The exe generated might not work. Or its just a wine issue. I'm not sure.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,17 @@
-echo "Compiling..."
-gcc -o out/casm.out src/main.c
-echo "Compiled!"
+echo -n "Do you want to compile it for linux or windows: "
+read linuxorwindows
+
+if [ $linuxorwindows == "linux" ]; then
+    echo "Compiling..."
+    gcc -o out/casm.out src/main.c
+    echo "Compiled!"
+elif [ $linuxorwindows == "windows" ]; then
+    echo "WARNING!
+This is experimental and either doesnt work with wine or doesnt work at all."
+    sleep 1
+    echo "Compiling..."
+    x86_64-w64-mingw32-gcc -o out/casm.exe src/main.c
+    echo "Compiled!"
+else
+    echo "Invalid option. Exiting..."
+fi
