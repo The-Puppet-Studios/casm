3b71d384a01a56740768bb3e9e5d9360dc3ba7fb
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-02-18 22:29:10 -0800

    Fixed the linux build script cuz it brokey
    idk how it worked before lmao

--- compile.sh
+++ compile.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 echo -n "Do you want to compile it for linux or windows: "
 read linuxorwindows
 
-if [ $linuxorwindows == "linux" ]; then
+if [ "$linuxorwindows" = "linux" ]; then
     echo "Compiling..."
     gcc -o out/casm.out src/main.c
     echo "Compiled!"
-elif [ $linuxorwindows == "windows" ]; then
+elif [ "$linuxorwindows" = "windows" ]; then
     echo "Compiling..."
     x86_64-w64-mingw32-gcc -o out/casm.exe src/main.c
     echo "Compiled!"
